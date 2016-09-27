951e6fdc1f3c3ef1016ff98dc689ac1204010b21
kachergis <george.kachergis@gmail.com>
2016-09-27 15:32:09 +0200

    logs a (new) run from best agent every N generations
    animation stuff is there but commented out (not finished yet, and
    should maybe be in a separate class/file)

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import alife
 import experiments
 
 
-#experiments.run_mult_experiments(lifetime=1000, num_cores=2, num_gens=100)
+experiments.run_mult_experiments(lifetime=1000, num_cores=2, num_gens=50)
 
-experiments.run_mult_experiments(lifetime=600, num_cores=7, num_gens=300)
+# want to automatically save the best agent every X generations, and be able
+# to run them again on the environment and output an animation
+
+#experiments.run_mult_experiments(lifetime=600, num_cores=7, num_gens=300)
